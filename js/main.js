3d8db353bb9e0e1c5526d1e821efb12e49d0658c
betanzos515 <betanzos515@gmail.com>
2018-10-16 17:05:29 -0500

    Initial commit
    Privado

---
+++ js/main.js
@@ -0,0 +1,18 @@
+(function(){
+	"use strict";
+
+	document.addEventListener("DOMContentLoaded", function(){
+		//campos datos usuario
+
+		var nombre = document.getElementById('nombre');
+		var apellido = document.getElementById('apellido');
+		var email = document.getElementById('email');
+
+		//campus pases 
+		var pase_dia = document.getElementById('pase_dia');
+		var pase_dosdias = document.getElementById('pase_dosdias');
+		var pase_completo = document.getElementById('pase_completo');
+
+		var calcular = document.getElementById('error');
+	});
+})();
